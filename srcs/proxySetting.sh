ebc0196d597f1111218fc27e18e0700c8290072b
deco-L <scotchwitchbroom@gmail.com>
2025-04-15 03:43:46 +0900

    🐛 database fixed

--- srcs/proxySetting.sh
+++ srcs/proxySetting.sh
@@ -4,14 +4,14 @@ ENV_FILE="./srcs/.env"
 HTTP_PROXY_VALUE=$(env | grep "^HTTP_PROXY=" | cut -d '=' -f 2)
 HTTPS_PROXY_VALUE=$(env | grep "^HTTPS_PROXY=" | cut -d '=' -f 2)
 
-if [[ -f "$ENV_FILE" ]]; then
-  grep -v "^HTTP_PROXY=" $ENV_FILE | grep -v "^HTTPS_PROXY=" > $ENV_FILE
-fi
+sed -i '/# proxy setting/d' "$ENV_FILE"
+sed -i '/^HTTP_PROXY=/d' "$ENV_FILE"
+sed -i '/^HTTPS_PROXY=/d' "$ENV_FILE"
 
 {
-  echo "# proxy setting" >> $ENV_FILE
-  echo "HTTP_PROXY=$HTTP_PROXY_VALUE" >> $ENV_FILE
-  echo "HTTPS_PROXY=$HTTPS_PROXY_VALUE" >> $ENV_FILE
-}
+  echo "# proxy setting"
+  echo "HTTP_PROXY=$HTTP_PROXY_VALUE"
+  echo "HTTPS_PROXY=$HTTPS_PROXY_VALUE"
+} >> "$ENV_FILE"
 
 printf "\033[38;5;47mProxy setting is done.\033[0m\n"
--- srcs/requirements/mariadb/init.sql
+++ srcs/requirements/mariadb/init.sql
@@ -1,6 +1,9 @@
-CREATE DATABASE IF NOT EXISTS wordpress;
-CREATE USER 'wp_root'@'%' IDENTIFIED BY '/run/secrets/db_root_password';
-CREATE USER 'wp_user'@'%' IDENTIFIED BY '/run/secrets/db_user_password';
-GRANT ALL PRIVILEGES ON wordpress.* TO 'wp_root'@'%' WITH GRANT OPTION;
-GRANT SELECT, INSERT, UPDATE, DELETE ON wordpress.* TO 'wp_user'@'%';
+CREATE DATABASE wordpress;
+
+-- CREATE USER wp_root@'%' IDENTIFIED BY 'wp_root_password';
+-- GRANT ALL PRIVILEGES ON wordpress.* TO wp_root@'%' WITH GRANT OPTION;
+
+CREATE USER 'wp_user'@'%' IDENTIFIED BY 'wp_user_password';
+GRANT ALL PRIVILEGES ON *.* TO 'wp_user'@'%' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES;
